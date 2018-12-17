285d4d3112405934d686d57e51ec2df234e2b5c1
west040594 <semen253@mail.ru>
2018-12-17 18:15:44 +0300

    add kafka docker

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tstufileuploader
 tstu file uploader
+1. git clone
+2. docker-compose up
+3. run spring boot app - filerecieverclient
+
+kafka - localost:29092
+Kafka UI - localhost:8000
+fileuploadserver - localhost:8085
+
+
--- filerecieverclient/src/main/java/com/tstu/filerecieverclient/messaging/ImageListener.java
+++ filerecieverclient/src/main/java/com/tstu/filerecieverclient/messaging/ImageListener.java
@@ -2,18 +2,20 @@ package com.tstu.filerecieverclient.messaging;
 
 import com.tstu.filerecieverclient.service.filestorage.FileStorageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-
+@Slf4j
 public class ImageListener {
 
     private final FileStorageService fileStorageService;
 
     @KafkaListener(topics="images")
     public void handle(String imageUrl) {
+        log.info("Доступно новое изображение по ссылке {}. Начинается скачивание...", imageUrl);
         fileStorageService.storeFileFromUrl(imageUrl);
     }
 }
